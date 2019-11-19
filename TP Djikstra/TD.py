1838fe0d5e609d31ac1c50bf2a1700e032411c9b
benjivou <benjivou2@gmail.com>
2019-11-19 08:52:01 +0100

    Debut TD

--- TP Djikstra/TD.py
+++ TP Djikstra/TD.py
@@ -1,11 +1,46 @@
 import os
 import math
 import time
+from tkinter import *
 
-file_graph = 'Arcs.csv'
+
+
+
+file_arc = 'Arcs.csv'
+file_noeuds = 'Noeuds.csv'
+
+sommet_depart = 23160
+sommet_destination = 27195
+degre_version_radian = math.pi/180.0
 # Read the doc
-TheGraph = open(file_graph, 'r')
-all_arcs = TheGraph.readlines()
+TheArcs = open(file_arc, 'r')
+TheNoeuds = open(file_noeuds, 'r')
+
+all_arcs = TheArcs.readlines()
+all_noeuds = TheNoeuds.readlines()
+
+TheNoeuds.close()
+
+# Noeuds
+Longitude = []
+Latitudes = []
+
+for un_noeud in all_noeuds:
+    ce_noeud = un_noeud.split("\t")
+    noeud = int(ce_noeud[0])
+    Long = float(ce_noeud[1])
+    Long = Long*degre_version_radian
+    Longitude.append(Long)
+
+    Lat = float(ce_noeud[2].strip("\n"))
+    Lat = Lat *degre_version_radian
+    Latitudes.append(Lat)
+
+minLat = min(Latitudes)
+maxLat = max(Latitudes)
+minLong = min(Longitude)
+maxLong = max(Longitude)
+NbNoeuds = len(Longitude)
 
 # Remove tab
 Origin = []
@@ -35,4 +70,39 @@ for u in range(0, NbVertices):
     suiv[orig].append(dest)
     prec[dest].append(orig)
 
+def TraceCercle(j,couleur,rayon):
+    x=(Longitude[j]-minLong)*ratioWidth + border
+    y=(Latitudes[j]-minLat)*ratioHeight+ border
+    y=winHeight-y
+    can.create_oval(x-rayon, y-rayon, x+rayon, y+rayon, outline = couleur, fill = couleur)
+
+fen = Tk()
+fen.title('Carte de Paris')
+coul_fond = "grey"   #['purple','cyan','maroon','green','red','blue','orange','yellow']
+coul_noeud = "black"
+
+Delta_Long = maxLong-minLong
+Delta_Lat = maxLat-minLat
+
+border = 20         # taille en px des bords
+winWidth_int = 900
+winWidth = winWidth_int+2*border     # largeur de la fenetre
+winHeight_int = Delta_Lat*(winWidth_int/0.8)/Delta_Long
+winHeight = winHeight_int+2*border     # hauteur de la fenetre : recalculee en fonction de la taille du graphe
+ratio= 1.0          # rapport taille graphe / taille fenetre
+ratioWidth = winWidth_int/Delta_Long       #  rapport largeur graphe/ largeur de la fenetre
+ratioHeight = winHeight_int/Delta_Lat       #  rapport hauteur du graphe hauteur de la fenetre
+
+can = Canvas(fen, width = winWidth, height = winHeight, bg =coul_fond)
+can.pack(padx=5,pady=5)
+
+#  cercles
+rayon_noeud = 1  # rayon pour dessin des points
+rayon_od = 5     # rayon pour origine et destination
+for i in range(0,NbNoeuds):
+    TraceCercle(i,coul_noeud,rayon_noeud)
+TraceCercle(sommet_depart,'green',rayon_od)
+TraceCercle(sommet_destination,'red',rayon_od)
+
+can.mainloop()
 print(NBArcs)
